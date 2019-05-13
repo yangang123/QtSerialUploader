e5a8b3e18c4926e29c23eeb0958b800db5b6ad40
yangang <320054593@qq.com>
2019-05-13 20:08:41 +0800

    支持magicbook笔记本显示，否则会出现窗口字体错位的问题

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 
 #include <QApplication>
 #include "mainwindow.h"
+#include <QFont>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    //设置字体，避免在小尺寸电脑上，窗口显示错位
+    QFont font(QStringLiteral("新宋体"));
+    font.setPixelSize(12);
+    a.setFont(font);
     MainWindow::getInstance()->show();
 
     return a.exec();
